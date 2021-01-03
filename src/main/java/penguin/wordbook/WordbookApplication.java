075060f3d760a5eb973da53988a3bb43099c2052
penguin418 <penguin418@naver.com>
2021-01-03 12:18:55 +0900

    init

---
+++ src/main/java/penguin/wordbook/WordbookApplication.java
@@ -0,0 +1,13 @@
+package penguin.wordbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/penguin/wordbook/WordbookApplicationTests.java
@@ -0,0 +1,13 @@
+package penguin.wordbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
