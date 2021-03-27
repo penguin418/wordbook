7200a20a30d8bd9855cc16e2152dd72ec42afd52
penguin418 <penguin418@naver.com>
2021-03-27 13:01:36 +0900

    feat: Use jpa interface to simplify repository classes

--- src/main/java/penguin/wordbook/repository/WordbookRepository.java
+++ src/main/java/penguin/wordbook/repository/WordbookRepository.java
@@ -1,18 +1,7 @@
 package penguin.wordbook.repository;
 
-import org.springframework.stereotype.Repository;
-import penguin.wordbook.domain.QA;
+import org.springframework.data.jpa.repository.JpaRepository;
 import penguin.wordbook.domain.Wordbook;
 
-import java.util.List;
-import java.util.Optional;
-
-@Repository
-public interface WordbookRepository {
-    Wordbook save(Wordbook wordbook);
-    boolean update(Wordbook wordbook);
-    Optional<Wordbook> findById(Long id);
-    List<Wordbook> findAll();
-    boolean delete(Wordbook wordbook);
-    boolean deleteById(Long id);
+public interface WordbookRepository extends JpaRepository<Wordbook, Long> {
 }
