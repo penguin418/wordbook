da04b5cf3cbf9c9b1e70dfc1dbbfeab0d92d52e3
penguin418 <penguin418@naver.com>
2021-04-02 21:15:48 +0900

    doc: Add comment to CookieUtil

--- src/main/java/penguin/wordbook/util/CookieUtil.java
+++ src/main/java/penguin/wordbook/util/CookieUtil.java
@@ -6,6 +6,13 @@ import javax.servlet.http.HttpServletResponse;
 
 public class CookieUtil {
 
+    /**
+     * 쿠키 추가
+     * @param res {HttpServletResponse} 리스폰스
+     * @param name {String} 쿠키 이름
+     * @param value {String} 쿠키 값
+     * @param expireAt {Long} 만료
+     */
     public static void addCookie(HttpServletResponse res, String name, String value, Long expireAt){
         Cookie cookie = new Cookie(name, value);
         cookie.setHttpOnly(true);
@@ -13,14 +20,25 @@ public class CookieUtil {
         cookie.setPath("/");
         res.addCookie(cookie);
     }
-
+    /**
+     * 쿠키 삭제
+     * @param res {HttpServletResponse} 리스폰스
+     * @param name {String} 쿠키 이름
+     */
     public static void deleteCookie(HttpServletResponse res, String name){
         Cookie cookie = new Cookie(name, null);
         cookie.setMaxAge(0);
         cookie.setPath("/");
         res.addCookie(cookie);
     }
-
+    /**
+     * 쿠키 조회
+     * FIXME: optional 로 바꾸는 게 맞는 거 같은데
+     *
+     * @param req {HttpServletRequest} 리퀘스트
+     * @param name {String} 쿠키 이름
+     * @return Cookie 성공 시 쿠키, 실패 시 null
+     */
     public static Cookie getCookie(HttpServletRequest req, String name){
         final Cookie[] cookies = req.getCookies();
         if (cookies != null)
