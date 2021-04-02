3235e9ea67119663ee1656adf1585e9941513267
penguin418 <penguin418@naver.com>
2021-04-02 21:38:59 +0900

    chores: resolve conflict

--- src/main/java/penguin/wordbook/service/WordbookService.java
+++ src/main/java/penguin/wordbook/service/WordbookService.java
@@ -20,31 +20,52 @@ import java.util.stream.Collectors;
 public class WordbookService {
 
     private final WordbookRepository wordbookRepository;
-
     private final QARepository qaRepository;
-
     private final WordbookMapper wordbookMapper;
 
+    /**
+     * 생성
+     * @param dto {WordbookCreateDto} 생성 요청
+     * @return WordbookDetailDto 생성된 wordbook
+     */
     public WordbookDetailDto create(WordbookCreateDto dto) {
         Wordbook wordbook = wordbookMapper.mapToDetailDto(dto);
         wordbookRepository.save(wordbook);
         return wordbookMapper.mapToDetailDto(wordbook);
     }
 
+    /**
+     * 조회
+     * @param id {Long} wordbook 아이디
+     * @return WordbookDetailDto qa가 담긴 자세한 wordbook
+     */
     public WordbookDetailDto findOne(Long id) {
         Wordbook wordbook = wordbookRepository.findById(id).orElseThrow(EntityNotFoundException::new);
         return wordbookMapper.mapToDetailDto(wordbook);
     }
 
+    /**
+     * 전체 조회
+     * @return List 간단한 wordbook 내역
+     * TODO: pagination 구현 후 return 타입 WordbooksResultSetDto로 교체
+     */
     public List<WordbookItemDto> findAll() {
         return wordbookRepository.findAll().stream().map(wordbookMapper::mapToItemDto).collect(Collectors.toList());
     }
 
+    /**
+     * 갱신
+     * @param dto {WordbookUpdateDto}
+     */
     public void update(WordbookUpdateDto dto) {
         Wordbook wordbook = wordbookMapper.mapToDetailDto(dto);
         wordbookRepository.save(wordbook);
     }
 
+    /**
+     * 삭제
+     * @param id {Long}
+     */
     public void removeById(Long id) {
         wordbookRepository.deleteById(id);
     }
