c1312cf5961372c00312cf6360dbde08aecfeb00
penguin418 <penguin418@naver.com>
2021-04-07 15:51:52 +0900

    feat(my_wordbook): Add getMyWordbookList to AccountApiController

--- src/main/java/penguin/wordbook/repository/WordbookRepository.java
+++ src/main/java/penguin/wordbook/repository/WordbookRepository.java
@@ -3,10 +3,12 @@ package penguin.wordbook.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import penguin.wordbook.domain.Account;
 import penguin.wordbook.domain.Wordbook;
 
 import java.util.List;
 
 @Repository
 public interface WordbookRepository extends JpaRepository<Wordbook, Long> {
+    public List<Wordbook> findByAccountAccountId(Long accountId);
 }
