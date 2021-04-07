10e24becf5c84637e2028f13eb367336272a96b9
penguin418 <penguin418@naver.com>
2021-04-07 14:46:52 +0900

    feat(add_account): Make logout button hide

--- src/main/java/penguin/wordbook/util/CookieUtil.java
+++ src/main/java/penguin/wordbook/util/CookieUtil.java
@@ -1,5 +1,7 @@
 package penguin.wordbook.util;
 
+import org.springframework.http.ResponseCookie;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,11 +16,13 @@ public class CookieUtil {
      * @param expireAt {Long} 만료
      */
     public static void addCookie(HttpServletResponse res, String name, String value, Long expireAt){
-        Cookie cookie = new Cookie(name, value);
-        cookie.setHttpOnly(true);
-        cookie.setMaxAge(Math.toIntExact(expireAt));
-        cookie.setPath("/");
-        res.addCookie(cookie);
+        ResponseCookie cookie = ResponseCookie.from(name, value)
+                .sameSite("None")
+                .secure(true)
+                .path("/")
+                .maxAge(Math.toIntExact(expireAt))
+                .build();
+        res.addHeader("Set-Cookie", cookie.toString());
     }
     /**
      * 쿠키 삭제
